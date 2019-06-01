786dcc76ba831277190f1a684f42b5987460f81b
SeungKi Kim <tttuuu888@gmail.com>
2019-06-01 20:02:23 +0900

    Fix "ivy-posframe copies prompt text as kill" #41

--- ivy-posframe.el
+++ ivy-posframe.el
@@ -268,7 +268,9 @@ This variable is useful for `ivy-posframe-read-action' .")
         (remove-text-properties 0 (length prompt) '(read-only nil) prompt)
         (with-current-buffer ivy-posframe-buffer
           (goto-char (point-min))
-          (kill-line 1)
+          (delete-region
+           (point)
+           (save-excursion (move-end-of-line 1) (point)))
           (insert prompt "  \n")
           (add-text-properties point (1+ point) '(face ivy-posframe-cursor)))))))
 
