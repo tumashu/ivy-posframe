d89a64fc8c68579a58c126b8b7be686b0a1690c5
conao3 <conao3@gmail.com>
2019-07-01 05:22:43 +0900

    remove unneeded arguments check
    All advice functions should be used in a predetermined manner
    and only for predetermined purposes.
    It is therefore reasonable not to check the arguments.
    
    As proof of this, no other advising function performs the same checks.
    Remove the non-essential code and focus only on what we want to focus on.

--- ivy-posframe.el
+++ ivy-posframe.el
@@ -469,13 +469,7 @@ selection, non-nil otherwise."
 
 (defun ivy-posframe--posframe-p-advice (advice-fn &rest args)
   "Advice function of ADVICE-FN, used to bypass the advice from
-`ivy-posframe-advice-alist' if the posframe cannot be displayed.
-
-ADVICE-FN should be a value from `ivy-posframe-advice-alist', but
-the function only errors if ARGS is empty. There should at least be
-the advised function there (a key from `ivy-posframe-advice-alist')."
-  (unless (< 0 (length args))
-    (error "This function should advise an advice, so args should be at least a key from ivy-posframe-advice-alist"))
+`ivy-posframe-advice-alist' if the posframe cannot be displayed."
   (if (display-graphic-p)
       (apply advice-fn args)
     (apply (car args) (cdr args))))
