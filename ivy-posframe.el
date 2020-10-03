ac0c785e09bfbcedb6622a0daf0a3e7f946b3468
Feng Shu <tumashu@163.com>
2020-10-04 06:02:12 +0800

    * ivy-posframe.el: version 0.3.0

--- ivy-posframe.el
+++ ivy-posframe.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Feng Shu <tumashu@163.com>
 ;; Maintainer: Naoya Yamashita <conao3@gmail.com>
 ;; URL: https://github.com/tumashu/ivy-posframe
-;; Version: 0.2.0
+;; Version: 0.3.0
 ;; Keywords: abbrev, convenience, matching, ivy
 ;; Package-Requires: ((emacs "26.0")(posframe "0.1.0")(ivy "0.11.0"))
 
